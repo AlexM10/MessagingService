d6eb19f0bd7f980f166f815da4e46f045023a10c
AlexM10 <alexmongait2@gmail.com>
2020-10-10 01:34:45 +0300

    added check first = false

--- Model/database_setup.py
+++ Model/database_setup.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from Model.Models import Base
+
 engine = create_engine('sqlite:///Model/message_handler.db')
 Base.metadata.bind = engine
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine, checkfirst=False)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
